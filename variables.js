d773aacbfe7fb772c2994294670f71148ba46c36
Akshat Kumar <akshat.heic777@gmail.com>
2025-11-02 22:38:32 +0530

    Just checkin the bs

--- variables.js
+++ variables.js
@@ -2,7 +2,7 @@ let userName = "akshat";
 let age = 21;
 let isLoggedIn = true;
 let hobbies =["cricket" , "gym" , "coding"];
-hobbies[2] = "music"
+hobbies[2] = "music";
 let userProfile = {
     name : "aks",
     age : 21,
