7af7d1e2a18b97c991a6b0bba6a27db77acd3a84
Akshat Kumar <akshat.heic777@gmail.com>
2025-11-02 22:29:55 +0530

    Initial commit : Adding Javascript basics

---
+++ variables.js
@@ -0,0 +1,14 @@
+let userName = "akshat";
+let age = 21;
+let isLoggedIn = true;
+let hobbies =["cricket" , "gym" , "coding"];
+hobbies[2] = "music"
+let userProfile = {
+    name : "aks",
+    age : 21,
+    isStudent : true,
+    isHandsome : true
+
+}
+console.log(userProfile);
+console.log(hobbies);
